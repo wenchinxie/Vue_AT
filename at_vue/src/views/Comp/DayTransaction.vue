2147edf6a6c14e2d527c21d76565a1362dee84cc
Xiexieni9527 <wenchin.xie@gmail.com>
2023-05-21 16:12:15 +0800

    [DayTrans]

--- at_vue/src/views/Comp/DayTransaction.vue
+++ at_vue/src/views/Comp/DayTransaction.vue
@@ -9,42 +9,52 @@
 
 <script>
 import Plotly from 'plotly.js/dist/plotly' 
+import axios from 'axios'
 
 export default {
-    name:'LineChart',
+    name:'DayTransactionChart',
     data() {
         return {
-            non_trading_dates : ['2017-01-21', '2017-01-22'],
-            row:2
+            day_trans:''
         } 
     },
 
     mounted(){
-        this.getProductData()
+        this.getDayTrans()
     },
 
   methods: {
-        async getProductData(){
+        async getDayTrans(){
 
-
+            const stockid = this.$route.params.stockid
+            await axios
+            .get(`/api/v1/comp/${stockid}/DayTransaction/`)
+            .then(response => {
+                this.day_trans = response.data
+                
+            })
+            .catch(error => {
+                console.log(error)
+            })
+            console.log(this.day_trans)
 
             var candlestick_trace = {
   
-            x: ['2017-01-17', '2017-01-18', '2017-01-19', '2017-01-20', '2017-01-23', '2017-01-24', '2017-01-25', '2017-01-26', '2017-01-27', '2017-01-30', '2017-01-31', '2017-02-01', '2017-02-02', '2017-02-03', '2017-02-06', '2017-02-07', '2017-02-08', '2017-02-09', '2017-02-10'], 
+            x: this.day_trans.Date, 
             
-            close: [120, 119.989998, 119.779999, 120, 120.080002, 119.970001, 121.879997, 121.940002, 121.949997, 121.629997, 121.349998, 128.75, 128.529999, 129.080002, 130.289993, 131.529999, 132.039993, 132.419998, 132.119995], 
+            close:this.day_trans.Close,
             
             decreasing: {line: {color: '#00CB19'}}, 
             
-            high: [120.239998, 120.5, 120.089996, 120.449997, 120.809998, 120.099998, 122.099998, 122.440002, 122.349998, 121.629997, 121.389999, 130.490005, 129.389999, 129.190002, 130.5, 132.089996, 132.220001, 132.449997, 132.940002], 
-            
+            high: this.day_trans.Max,
+
             increasing: {line: {color: '#FF0000'}}, 
             
             line: {color: 'rgba(31,119,180,1)'}, 
             
-            low: [118.220001, 119.709999, 119.370003, 119.730003, 119.769997, 119.5, 120.279999, 121.599998, 121.599998, 120.660004, 120.620003, 127.010002, 127.779999, 128.160004, 128.899994, 130.449997, 131.220001, 131.119995, 132.050003], 
-            
-            open: [118.339996, 120, 119.400002, 120.449997, 120, 119.550003, 120.419998, 121.669998, 122.139999, 120.93, 121.150002, 127.029999, 127.980003, 128.309998, 129.130005, 130.539993, 131.350006, 131.649994, 132.460007], 
+            low: this.day_trans.Min,
+
+            open: this.day_trans.Open,
             
             type: 'candlestick',
 
@@ -55,19 +65,20 @@ export default {
             name: 'candlestick'
 
             };
+            console.log(this.day_trans.TradeVolume)
             var trace2 = {
-                x: ['2017-01-17', '2017-01-18', '2017-01-19', '2017-01-20', '2017-01-23', '2017-01-24', '2017-01-25', '2017-01-26', '2017-01-27', '2017-01-30', '2017-01-31', '2017-02-01', '2017-02-02', '2017-02-03', '2017-02-06', '2017-02-07', '2017-02-08', '2017-02-09', '2017-02-10'], 
-                y: [1200, 1190.989998, 1190.779999, 1200, 120.080002, 119.970001, 121.879997, 121.940002, 121.949997, 121.629997, 121.349998, 128.75, 128.529999, 129.080002, 130.289993, 131.529999, 132.039993, 132.419998, 132.119995], 
-                name: 'LA Zoa',
+                x: this.day_trans.Date,
+                y: this.day_trans['TradeVolume'], 
+                name: '交易量',
                 type: 'bar',
                 xaxis:'x',
                 yaxis:'y2',
                 };
                 
             var trace3 = {
-                x: ['2017-01-17', '2017-01-18', '2017-01-19', '2017-01-20', '2017-01-23', '2017-01-24', '2017-01-25', '2017-01-26', '2017-01-27', '2017-01-30', '2017-01-31', '2017-02-01', '2017-02-02', '2017-02-03', '2017-02-06', '2017-02-07', '2017-02-08', '2017-02-09', '2017-02-10'], 
-                y: [900, 1090.989998, 1390.779999, 1400, 150.080002, 219.970001, 121.879997, 121.940002, 121.949997, 121.629997, 121.349998, 128.75, 128.529999, 129.080002, 130.289993, 131.529999, 132.039993, 132.419998, 132.119995], 
-                name: 'LA Zoo',
+                x: this.day_trans.Date,
+                y: this.day_trans['DayTradeVolume'], 
+                name: '當沖量',
                 type: 'bar',
                 xaxis:'x',
                 yaxis:'y2',
@@ -79,7 +90,7 @@ export default {
 
             var layout = {       
                 grid: {
-                    rows: 3,
+                    rows: 2,
                     columns: 1,
                     roworder:'top to bottom',
                     pattern:'coupled',
@@ -91,7 +102,7 @@ export default {
                     title: 'Date',
                     rangeslider: {visible: false},
                     rangebreaks: [{
-                        values: this.non_trading_dates
+                        values: this.day_trans['Missing Date']
                     }],
                     rangeselector: {
                         x: 0,
@@ -122,7 +133,7 @@ export default {
                     matches: 'x',
                     anchor: 'x',
                     rangebreaks: [{
-                        values: this.non_trading_dates
+                        values: this.day_trans['Missing Date']
                     }],
                 },
                 yaxis: {
@@ -243,14 +254,9 @@ export default {
             ]
         });
         });
-
-
-
-
     }
     ,
     async getDayTransaction1(){
-
 
         var trace1 = {
         x: [1, 2, 3, 4],
@@ -339,6 +345,5 @@ export default {
             
 }
 }
-
 
 </script>
